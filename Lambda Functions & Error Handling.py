fe646dc7f8bae96d0e8d7047754def39f3d1a4ff
Jill S. Budden <jill.budden@gmail.com>
2017-02-14 12:30:43 -0600

    Lambda Functions & Error Handling

---
+++ Lambda Functions & Error Handling.py
@@ -0,0 +1,87 @@
+##########################lamda functions#####################
+#lamda functions simplify full functions - they do not use a "def"
+
+#####map() and lambda functions#####
+#map() applies a function over an oject, such as a list
+
+# Create a list of strings: spells
+spells = ['protego', 'accio', 'expecto patronum', 'legilimens']
+
+# Use map() to apply a lambda function over spells: shout_spells
+shout_spells = map(lambda item: item + '!!!', spells)
+
+# Convert shout_spells to a list: shout_spells_list
+shout_spells_list = list(shout_spells)
+
+# Convert shout_spells into a list and print it
+print(shout_spells_list)
+
+
+#####filter() and lambda functions#####
+#filter() offers a way to filter out elements from a list that do not satisfy certain criteria
+
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
+
+# Use filter() to apply a lambda function over fellowship: result
+result = filter(lambda member: len(member) > 6, fellowship)
+
+# Convert result to a list: result_list
+result_list = list(result)
+
+# Convert result into a list and print it
+print(result_list)
+
+
+##########################Error handling#####################
+#Anticipating ways other people may misuse your function
+
+#####use a try-except block#####
+# Define shout_echo
+def shout_echo(word1, echo=1):
+    """Concatenate echo copies of word1 and three
+    exclamation marks at the end of the string."""
+
+    # Initialize empty strings: echo_word, shout_words
+    echo_word = ''
+    shout_words = ''
+
+    # Add exception handling with try-except
+    try:
+        # Concatenate echo copies of word1 using *: echo_word
+        echo_word = word1 * echo
+
+        # Concatenate '!!!' to echo_word: shout_words
+        shout_words = echo_word + '!!!'
+    except:
+        # Print error message
+        print("word1 must be a string and echo must be an integer.")
+
+    # Return shout_words
+    return shout_words
+
+# Call shout_echo
+shout_echo("particle", echo="accelerator")
+
+
+#####Raising an error#####
+# Define shout_echo
+def shout_echo(word1, echo=1):
+    """Concatenate echo copies of word1 and three
+    exclamation marks at the end of the string."""
+
+    # Raise an error with raise
+    if echo < 0:
+        raise ValueError('echo must be greater than 0')
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1 * echo
+
+    # Concatenate '!!!' to echo_word: shout_word
+    shout_word = echo_word + '!!!'
+
+    # Return shout_word
+    return shout_word
+
+# Call shout_echo
+shout_echo("particle", echo=5)
