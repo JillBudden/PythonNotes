6f7bde18e9eb63482cad964f46b06aeabce2eb59
Jill S. Budden <jill.budden@gmail.com>
2017-02-14 12:31:13 -0600

    Python Data Science Toolbox 1: Functions

---
+++ Python Data Science Toolbox 1_Functions.py
@@ -0,0 +1,164 @@
+########################Writing your own Function###########################
+
+#####Function without a parameter#####
+# Define the function shout
+def shout():
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = 'congratulations' + '!!!'
+
+    # Print shout_word
+    print(shout_word)
+
+# Call shout
+shout()
+
+
+#####Function with 1 parameter#####
+# Define shout with the parameter, word
+def shout(word):
+    """Return a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = word + '!!!'
+
+    # Replace print with return
+    return(shout_word)
+
+# Pass 'congratulations' to shout: yell
+yell = shout('congratulations')
+
+# Print yell
+print(yell)
+
+
+#####Function with multiple parameters#####
+# Define shout with parameters word1 and word2
+def shout(word1, word2):
+    """Concatenate strings with three exclamation marks"""
+    # Concatenate word1 with '!!!': shout1
+    shout1 = word1 + '!!!'
+    
+    # Concatenate word2 with '!!!': shout2
+    shout2 = word2 + '!!!'
+    
+    # Concatenate shout1 with shout2: new_shout
+    new_shout = shout1 + shout2
+
+    # Return new_shout
+    return new_shout
+
+# Pass 'congratulations' and 'you' to shout(): yell
+yell = shout('congratulations', 'you')
+
+# Print yell
+print(yell)
+
+
+#####Nested Functions#####
+# Define echo
+def echo(n):
+    """Return the inner_echo function."""
+
+    # Define inner_echo
+    def inner_echo(word1):
+        """Concatenate n copies of word1."""
+        echo_word = word1 * n
+        return echo_word
+
+    # Return inner_echo
+    return inner_echo
+
+# Call echo: twice
+twice = echo(2)
+
+# Call echo: thrice
+thrice = echo(3)
+
+# Call twice() and thrice() then print
+print(twice('hello'), thrice('hello'))
+
+
+#####Functions with 1 default arguement#####
+# Define shout_echo
+def shout_echo(word1, echo=1):
+    """Concatenate echo copies of word1 and three
+     exclamation marks at the end of the string."""
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1*echo
+
+    # Concatenate '!!!' to echo_word: shout_word
+    shout_word = echo_word + '!!!'
+
+    # Return shout_word
+    return shout_word
+
+# Call shout_echo() with "Hey": no_echo
+no_echo = shout_echo("Hey")
+
+# Call shout_echo() with "Hey" and echo=5: with_echo
+with_echo = shout_echo("Hey", echo=5)
+
+# Print no_echo and with_echo
+print(no_echo)
+print(with_echo)
+
+
+#####Functions with multiple default arguments#####
+# Define shout_echo
+def shout_echo(word1, echo=1, intense=False):
+    """Concatenate echo copies of word1 and three
+    exclamation marks at the end of the string."""
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1 * echo
+
+    # Capitalize echo_word if intense is True
+    if intense is True:
+        # Capitalize and concatenate '!!!': echo_word_new
+        echo_word_new = echo_word.upper() + '!!!'
+    else:
+        # Concatenate '!!!' to echo_word: echo_word_new
+        echo_word_new = echo_word + '!!!'
+
+    # Return echo_word
+    return echo_word_new
+
+# Call shout_echo() with "Hey", echo=5 and intense=True: with_big_echo
+with_big_echo = shout_echo("Hey", echo=5, intense=True)
+
+# Call shout_echo() with "Hey" and intense=True: big_no_echo
+big_no_echo = shout_echo("Hey", intense=True)
+
+# Print values
+print(with_big_echo)
+print(big_no_echo)
+
+
+#####Functions with *args#####
+#flexible arguments enable you to pass a variable number of arguments to a function
+# Define gibberish
+def gibberish(*args):
+    """Concatenate strings in *args together."""
+
+    # Initialize an empty string: hodgepodge
+    hodgepodge = ""
+
+    # Concatenate the strings in args
+    for word in args:
+        hodgepodge += word
+
+    # Return hodgepodge
+    return hodgepodge
+
+# Call gibberish() with one string: one_word
+one_word = gibberish("luke")
+
+# Call gibberish() with five strings: many_words
+many_words = gibberish("luke", "leia", "han", "obi", "darth")
+
+# Print one_word and many_words
+print(one_word)
+print(many_words)
+
+
