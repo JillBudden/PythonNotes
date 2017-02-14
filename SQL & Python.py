8f8cc09e7a11adf188214024e814f0d901342b04
Jill S. Budden <jill.budden@gmail.com>
2017-02-14 12:29:43 -0600

    SQL & Python

---
+++ SQL & Python.py
@@ -0,0 +1,85 @@
+############################SQL & Python#############################
+#####Example 1#####
+# Import necessary module
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Open engine connection
+con = engine.connect()
+
+# Save the table names to a list: table_names
+table_names = engine.table_names()
+
+# Print the table names to the shell
+print(table_names)
+
+#####Example 2#####
+# Import necessary module
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute("SELECT LastName, Title FROM Employee")
+    df = pd.DataFrame(rs.fetchmany(size=3))
+    df.columns = rs.keys()
+
+# Print the length of the DataFrame df
+print(len(df))
+
+# Print the head of the DataFrame df
+print(df.head())
+
+#####Other example SQL pulls#####
+rs = con.execute('SELECT * FROM Employee WHERE EmployeeID >= 6')
+rs = con.execute('SELECT * FROM Employee ORDER BY BirthDate')
+
+
+#####Querying relational databases with PANDAS#####
+#####PANDAS, SQL, and output into a DataFreame with one line of code#####
+# Import packages
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Execute query and store records in DataFrame: df
+df = pd.read_sql_query('SELECT * FROM Album', engine)
+
+# Print head of DataFrame
+print(df.head())
+
+
+#####Inner Join#####
+import pandas as pd
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute("SELECT Title, Name FROM Album INNER JOIN Artist on Album.ArtistID = Artist.ArtistID")
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+
+# Print head of DataFrame df
+print(df.head())
+
+
+#####Filter your Inner Join#####
+# Execute query and store records in DataFrame: df
+df = pd.read_sql_query(
+    "SELECT * FROM PlaylistTrack INNER JOIN Track on PlaylistTrack.TrackId = Track.TrackId WHERE Milliseconds < 250000",
+    engine
+)
+
+# Print head of DataFrame
+print(df.head())
